183be8b6b1666585ab84ddd35acc8cf79925a013
Tung Nguyen <nh.tung3344@gmail.com>
2025-03-17 18:27:41 +0700

    update color

--- main.cpp
+++ main.cpp
@@ -113,7 +113,7 @@ void GameUpdate(){
     int row = mousePos.y / TILE_HEIGHT;
 
     if (gameState == MAIN_MENU) {
-        if (IsKeyPressed(KEY_ENTER)) {
+        if (GetKeyPressed() != 0) {
             gameState = PLAYING;
             GameReset();
         }
@@ -121,13 +121,14 @@ void GameUpdate(){
     }
 
     if (gameState == GAME_OVER) {
-        if (IsKeyPressed(KEY_ENTER)) {
+        if (GetKeyPressed() != 0) {
             isGameOver = false;
             gameState = MAIN_MENU;
             GameReset();
         }
         return;
     }
+
 
     if(gameState == PLAYING) { 
         if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
@@ -186,49 +187,93 @@ void RenderTile(sTile tile){
     int hoverCol = mousePos.x / TILE_WIDTH;
     int hoverRow = mousePos.y / TILE_HEIGHT;
 
+    Color MineColors[] = {
+        {0, 0, 0, 255},
+        {20, 150, 60, 255},     // 1 - Dark Green
+        {30, 70, 180, 255},     // 2 - Dark Blue
+        {40, 160, 160, 255},    // 3 - Teal
+        {200, 180, 40, 255},    // 4 - Golden Yellow
+        {200, 100, 20, 255},    // 5 - Burnt Orange
+        {180, 40, 40, 255},     // 6 - Deep Red
+        {100, 30, 120, 255},    // 7 - Dark Purple
+        {180, 40, 140, 255}     // 8 - Deep Magenta
+    };
+
     if(tile.isRevealed){
 
         if(tile.isMine) {
 
             DrawRectangle(tile.x * TILE_WIDTH, tile.y * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT, RED);
             DrawTexture(texture_bomb, tile.x * TILE_WIDTH, tile.y * TILE_HEIGHT, WHITE);
-            WaitTime(0.5);
-        } 
-        
-        else {
 
+        } 
+        else {
             if (((tile.x + tile.y) % 2 == 0)){
 
                 if (tile.nearbyMineCount > 0) {
-                    DrawRectangle(tile.x * TILE_WIDTH, tile.y * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT, DARK_BROWN);
-                    DrawText(TextFormat("%d", tile.nearbyMineCount), tile.x * TILE_WIDTH + 14, tile.y * TILE_HEIGHT + 5, TILE_HEIGHT - 4, DARKGRAY );
+                    DrawRectangle(tile.x * TILE_WIDTH, 
+                        tile.y * TILE_HEIGHT, 
+                        TILE_WIDTH, 
+                        TILE_HEIGHT, 
+                        DARK_BROWN
+                    );
+                    DrawText(
+                        TextFormat("%d", tile.nearbyMineCount),
+                        tile.x * TILE_WIDTH + 14, 
+                        tile.y * TILE_HEIGHT + 5, 
+                        TILE_HEIGHT - 4, 
+                        MineColors[tile.nearbyMineCount]
+                    );
                 } 
                 if (tile.nearbyMineCount == 0) {
-                    DrawRectangle(tile.x * TILE_WIDTH, tile.y * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT, DARK_BROWN);
+                    DrawRectangle(tile.x * TILE_WIDTH, 
+                        tile.y * TILE_HEIGHT, 
+                        TILE_WIDTH, 
+                        TILE_HEIGHT, 
+                        DARK_BROWN
+                    );
                 }
 
             } 
-
             else {
-
                 if (tile.nearbyMineCount > 0) {
-                    DrawRectangle(tile.x * TILE_WIDTH, tile.y * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT, LIGHT_BROWN);
-                    DrawText(TextFormat("%d", tile.nearbyMineCount), tile.x * TILE_WIDTH + 14, tile.y * TILE_HEIGHT + 5, TILE_HEIGHT - 4, DARKGRAY );
+                    DrawRectangle(tile.x * TILE_WIDTH, 
+                        tile.y * TILE_HEIGHT, 
+                        TILE_WIDTH, 
+                        TILE_HEIGHT, 
+                        LIGHT_BROWN
+                    );
+                    DrawText(
+                        TextFormat("%d", tile.nearbyMineCount),
+                        tile.x * TILE_WIDTH + 14, 
+                        tile.y * TILE_HEIGHT + 5, 
+                        TILE_HEIGHT - 4, 
+                        MineColors[tile.nearbyMineCount]
+                    );
                 } 
                 if (tile.nearbyMineCount == 0) {
-                    DrawRectangle(tile.x * TILE_WIDTH, tile.y * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT, LIGHT_BROWN);
+                    DrawRectangle(tile.x * TILE_WIDTH, 
+                        tile.y * TILE_HEIGHT, 
+                        TILE_WIDTH, 
+                        TILE_HEIGHT, 
+                        LIGHT_BROWN
+                    );
                 }
 
             }
         }
     } 
-
     if (tile.isFlagged){
-        DrawTexture(texture_flag, tile.x * TILE_WIDTH + 5, tile.y * TILE_HEIGHT + 5, WHITE);
+        DrawTexture(texture_flag, tile.x * TILE_WIDTH + 5,
+            tile.y * TILE_HEIGHT + 5, 
+            WHITE
+        );
     }  
-
     if (tile.x == hoverCol && tile.y == hoverRow) {
-        DrawRectangle(tile.x * TILE_WIDTH, tile.y * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT, Color{255, 255, 255, 100});
+        DrawRectangle(tile.x * TILE_WIDTH, tile.y * TILE_HEIGHT,
+            TILE_WIDTH, TILE_HEIGHT,
+            Color{255, 255, 255, 100}
+        );
     }
 
 
@@ -329,7 +374,7 @@ void ToggleFlag(int x, int y){
     if (grid[x][y].isRevealed){
         return;
     }
-
+    
     grid[x][y].isFlagged = !grid[x][y].isFlagged;
 
 }
@@ -337,9 +382,12 @@ void ToggleFlag(int x, int y){
 void RenderBoard(){
     for (int row = 0; row < ROWS; row++) {
         for (int col = 0; col < COLS; col++) {
-
             Color tileColor = ((row + col) % 2 == 0) ? LIGHT_GREEN : DARK_GREEN;
-            DrawRectangle(col * TILE_WIDTH, row * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT, tileColor);
+            DrawRectangle(col * TILE_WIDTH, row * TILE_HEIGHT, 
+                TILE_WIDTH, 
+                TILE_HEIGHT, 
+                tileColor
+            );
         }
     }
 }
@@ -347,13 +395,31 @@ void RenderBoard(){
 void RenderGameOver(){
     if (isGameOver) {
         DrawRectangle(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, Color{0, 0, 0, 100});
-        DrawText("Game Over!", SCREEN_WIDTH / 2 - MeasureText("Game Over!", 40) / 2, SCREEN_HEIGHT / 2 - 20, 40, RED);
-        DrawText("Press ENTER to return to Main Menu", SCREEN_WIDTH / 2 - MeasureText("Press ENTER to return to Main Menu", 20) / 2, SCREEN_HEIGHT / 2 + 20, 20, RED);
+        DrawText("Game Over!", 
+            SCREEN_WIDTH / 2 - MeasureText("Game Over!", 40) / 2,
+            SCREEN_HEIGHT / 2 - 20,
+            40,
+            RED
+        );
+        DrawText("Press Any Key to return to Main Menu",
+            SCREEN_WIDTH / 2 - MeasureText("Press Any Key to return to Main Menu", 20) / 2,
+            SCREEN_HEIGHT / 2 + 20, 20, RED
+        );
     }
 }
 
 void RenderMainMenu() {
     ClearBackground(RAYWHITE);
-    DrawText("Minesweeper", SCREEN_WIDTH / 2 - MeasureText("Minesweeper", 40) / 2, SCREEN_HEIGHT / 2 - 60, 40, DARKGRAY);
-    DrawText("Press ENTER to Start", SCREEN_WIDTH / 2 - MeasureText("Press ENTER to Start", 20) / 2, SCREEN_HEIGHT / 2, 20, DARKGRAY);
+    DrawText("Minesweeper", 
+        SCREEN_WIDTH / 2 - MeasureText("Minesweeper", 40) / 2, 
+        SCREEN_HEIGHT / 2 - 60, 
+        40, 
+        DARKGRAY
+    );
+    DrawText("Press Any Key to Start", 
+        SCREEN_WIDTH / 2 - MeasureText("Press Any Key to Start", 20) / 2, 
+        SCREEN_HEIGHT / 2, 
+        20, 
+        DARKGRAY
+    );
 }
