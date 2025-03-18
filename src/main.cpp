243b76640c5b70d2c8317e99b7528d1b08276cfa
Tung Nguyen <nh.tung3344@gmail.com>
2025-03-18 21:33:37 +0700

    Added header files + updates

---
+++ src/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include "game.h"
+
+using namespace std;
+
+Texture2D texture_flag;
+Texture2D texture_bomb;
+
+int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+bool isGameOver = false;
+
+sTile grid[COLS][ROWS];
+
+GameState gameState = MAIN_MENU;
+
+int main () {
+
+    cout << "Hello World!" << endl;
+
+
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Raylib:: Minesweeper");
+    SetTargetFPS(60);
+
+    GameStartUp();
+
+    while (!WindowShouldClose()){
+        
+        GameUpdate();
+
+        BeginDrawing(); 
+
+            GameRender();
+
+        EndDrawing();
+
+    }
+
+    GameShutDown();
+    CloseWindow();
+
+    return 0;
+}
